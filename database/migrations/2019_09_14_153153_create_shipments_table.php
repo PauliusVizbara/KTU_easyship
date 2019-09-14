d258e14beddbc0744506b1887c1b0843ffcccccb
pauliusvizbara <paulius1207@gmail.com>
2019-09-14 18:39:11 +0300

    feat: Shipment database migration

---
+++ database/migrations/2019_09_14_153153_create_shipments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShipmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shipments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('id');
+            $table->string('name');
+            $table->double('weight');
+            $table->string('address');
+            $table->double('lat');
+            $table->double('long');
+            $table->string('city');
+            $table->integer('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shipments');
+    }
+}
